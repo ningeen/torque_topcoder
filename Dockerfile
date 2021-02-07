3bf21ada34902c1b3eca4de749c1a658e5a909a0
ningeen <ningenkun@gmail.com>
2021-02-08 01:56:25 +0500

    Pub 75.3

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM pytorch/pytorch:1.6.0-cuda10.1-cudnn7-runtime
 
 RUN mkdir /code
 COPY ["./predict_test.py", "./cosine_annearing_with_warmup.py", "./mobilenetv3.py", "./pytorch_dataset.py", "./pytorch_model.py", "./read_and_get_mel.py", "./proj_config.yaml", "./requirements.txt", "./train.py", "./train.sh", "./pred.sh", "/code/"]
-COPY ./model/right_mixup/* /code/weights/
+COPY ./model/origin_2ch/* /code/weights/
 RUN pip install -r /code/requirements.txt && apt update && apt-get -y install libsndfile1 && chmod +x /code/train.sh && chmod +x /code/pred.sh
--- predict_test.py
+++ predict_test.py
@@ -42,14 +42,31 @@ data, mel_logs, _ = get_data(
 print(f"Data loaded in {time.time() - start}s")
 start = time.time()
 
-num_folds = 10
+use_folds = np.array([1, 3, 7, 9])
+num_folds = len(use_folds)
 y_pred = np.zeros((len(mel_logs), 1))
+
+model = TorqueModel(
+    CONFIG['model_params']['out_features_conv'],
+    14,
+    CONFIG['model_params']['mid_features'],
+    n_channels=2
+)
+for i in use_folds:
+    fname = f'work_{CONFIG["experiment_name"][0]}_fold{i}.pt'
+    pretrained_path = os.path.join(MODEL_DIR, fname)
+    if not os.path.isfile(pretrained_path):
+        pretrained_path = search_file(fname)
+    model.load_state_dict(torch.load(pretrained_path, map_location=device))
+    prediction = get_prediction(data, mel_logs, model, device, n_feat=14, n_channels=2)
+    y_pred += prediction
+print("First model done")
 
 # model = TorqueModel(
 #     CONFIG['model_params']['out_features_conv'],
-#     14,
+#     9,
 #     CONFIG['model_params']['mid_features'],
-#     n_channels=2
+#     n_channels=1
 # )
 # for i in range(num_folds):
 #     fname = f'work_{CONFIG["experiment_name"][0]}_fold{i}.pt'
@@ -57,25 +74,9 @@ y_pred = np.zeros((len(mel_logs), 1))
 #     if not os.path.isfile(pretrained_path):
 #         pretrained_path = search_file(fname)
 #     model.load_state_dict(torch.load(pretrained_path, map_location=device))
-#     prediction = get_prediction(data, mel_logs, model, device, n_feat=14, n_channels=2)
+#     prediction = get_prediction(data, mel_logs, model, device, n_feat=9, n_channels=1)
 #     y_pred += prediction
-# print("First model done")
-
-model = TorqueModel(
-    CONFIG['model_params']['out_features_conv'],
-    9,
-    CONFIG['model_params']['mid_features'],
-    n_channels=1
-)
-for i in range(num_folds):
-    fname = f'work_{CONFIG["experiment_name"][0]}_fold{i}.pt'
-    pretrained_path = os.path.join(MODEL_DIR, fname)
-    if not os.path.isfile(pretrained_path):
-        pretrained_path = search_file(fname)
-    model.load_state_dict(torch.load(pretrained_path, map_location=device))
-    prediction = get_prediction(data, mel_logs, model, device, n_feat=9, n_channels=1)
-    y_pred += prediction
-print("Second model done")
+# print("Second model done")
 
 # for i in range(num_folds):
 #     fname = f'work_{CONFIG["experiment_name"][2]}_fold{i}.pt'
@@ -92,7 +93,7 @@ start = time.time()
 
 df = pd.read_csv(csv_path)
 df = df[['filename']]
-df['result'] = y_pred / num_folds # / len(CONFIG["experiment_name"])
+df['result'] = y_pred / num_folds  # / len(CONFIG["experiment_name"])
 df.to_csv(os.path.join(OUTPUT_DIR, 'result.csv'), index=False)
 
 print(f"Results saved in {time.time() - start}s")
